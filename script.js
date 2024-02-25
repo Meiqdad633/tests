b450b7d96997e37e054aac2ce59209e09c5a1187
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-25 12:08:27 +0330

    new

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-myarr=["meiqdad", 12,"fatemeh", 14]
-console.log(myarr[20])
+/* اعدادی به تعداد دلخواه از کاربر گرفته و آن را داخل ارایه ذخیره کند
+و سپس میانگین آنها را محاسبه کرده و نمایش دهد
+*/
 
-document.write(myarr[20])
-document.write(myarr[20])
+users=[]
+for(i= 0; i < 3; i++){
+    username = prompt("Enter your name")
+    while(username.length < 3 || username.length > 15 ){
+        username= prompt("Enter your name")
+    }
+
+    userfamily = prompt("Enter your lastname")
+    while(userfamily.length < 3 || userfamily.length > 15){
+        userfamily=  prompt("Enter your lastname")
+    }
+    
+    userage = prompt("Enter your age")
+    while(isNaN(userage) || c.length > 3){
+        userage=  prompt("Enter your age")
+    }
+
+    var user = {
+        name: username,
+        lastname: userfamily,
+        age : userage
+    }
+    users.push(user)
+
+}
+console.log(users)
