d1da68f8d837a9c4ffb079f22e154203a1f920df
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-25 23:43:47 +0330

    new

--- script.js
+++ script.js
@@ -9,3 +9,35 @@
 به طور دیفات 3 محصول با قیمت های مختلف به سلد خرید کاربر موجود باشد
 
 */
+products = [
+    {id:0, name: "pen",price:100},
+    {id:1, name: "book",price:200},
+    {id:2,name: "notebook",price:150},
+    {id:3,name: "pencil",price:170}
+]
+
+basket =[
+    {id:0, name: "pen",price:100}
+]
+
+productname = prompt("Please Enter the product name")
+
+
+
+stock = products.some(function(product){
+    if(product.name == product){
+        basket.shift(product)
+    }
+    return 
+
+
+}
+)
+
+if(stock){
+    alert(" کالا موحود میباشد")
+    console.log(basket)
+}
+
+
+
