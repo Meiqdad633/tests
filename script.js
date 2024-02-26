f5f55a5703a146f723f51594fce6d77594eee372
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-27 00:13:01 +0330

    new

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
-var product = [
-    {id:1, name: "Notebook", price: 50000},
-    {id:2, name: "Pencil", price: 20000},
-    {id:3, name: "Eraser", price: 10000},
-    {id:4, name: "Laptop", price: 35000000},
-    {id:5, name: "Phone", price: 10000000},
-    {id:6, name: "EarPod", price: 2000000},
+var telegramUsers = [
+    {id:1, name: "Fatemeh", age: 25},
+    {id:2, name: "Meiqdad", age: 26},
+    {id:3, name: "Zahra", age: 30},
+    {id:4, name: "Amir", age: 20},
+    {id:5, name: "Arman", age: 19},
 ];
-var cart = [
-    {id:1, name: "Eraser", price: 10000},
-    {id:2, name: "Notebook", price: 50000},
-];
-var productName = prompt("Enter a product you want:");
-var requestProduct;
-
-var result = product.some((item) => { if(item.name == productName){
-    requestProduct = item;
-    return true;}});
-
-if (result == true){
-    var newProduct = {
-        id: 3,
-        name: requestProduct.name,
-        price: requestProduct.price,
-    }
-    cart.push(newProduct);
-    var sum = 0
-    cart.forEach((item) => {
-        sum += item.price;
-    })
-    console.log(cart);
-    alert("Totalprice:" +sum);
+if (telegramUsers.every((item) => {return item.age>18})){
+    console.log("میتوانید وارد تماس کنفرانسی شوید");
 } else {
-    console.log("موجودی تمام شده");
+    console.log("اجازه ایجاد تماس کنفرانسی را ندارید");
 }
