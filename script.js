7a2c30b9abb89758d37fa3903f3866ad65dc56fe
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-26 23:50:36 +0330

    new

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
-/*
-محصولات یک فروشگاه انلاین را در دیتابس ذخیره کرده و ارایه ای برای سبد خرید کاربر داشته باشید 
-و از کاربر اسم محصولی را که قصد اضافه کردن به سبد خرید دارد دریافت کنید
-اگر محصول درخواستی کاربر به انبار فروشگاه شما موجود بود آن را به سبد خرید کاربر اضافهکنید
-و سپس مبلغ کل سبد خرید را محاسبه کرده و به کاربر نمایش دهید
-درغیر این صورت به کاربر پیغام اتمام موجودی نمایش دهید
-نکته
-بعد از اضافه محصول به سبد خرید سبد را در کنسول نمایش دهید
-به طور دیفات 3 محصول با قیمت های مختلف به سلد خرید کاربر موجود باشد
+var product = [
+    {id:1, name: "Notebook", price: 50000},
+    {id:2, name: "Pencil", price: 20000},
+    {id:3, name: "Eraser", price: 10000},
+    {id:4, name: "Laptop", price: 35000000},
+    {id:5, name: "Phone", price: 10000000},
+    {id:6, name: "EarPod", price: 2000000},
+];
+var cart = [
+    {id:1, name: "Eraser", price: 10000},
+    {id:2, name: "Notebook", price: 50000},
+];
+var productName = prompt("Enter a product you want:");
+var requestProduct;
 
-*/
-products = [
-    {id:0, name: "pen",price:100},
-    {id:1, name: "book",price:200},
-    {id:2,name: "notebook",price:150},
-    {id:3,name: "pencil",price:170}
-]
+var result = product.some((item) => { if(item.name == productName){
+    requestProduct = item;
+    return true;}});
 
-basket =[
-    {id:0, name: "pen",price:100}
-]
-
-productname = prompt("Please Enter the product name")
-
-
-
-stock = products.some(function(product){
-    if(product.name == product){
-        basket.shift(product)
+if (result == true){
+    var newProduct = {
+        id: 3,
+        name: requestProduct.name,
+        price: requestProduct.price,
     }
-    return 
-
-
+    cart.push(newProduct);
+    var sum = 0
+    cart.forEach((item) => {
+        sum += item.price;
+    })
+    console.log(cart);
+    alert("Totalprice:" +sum);
+} else {
+    console.log("موجودی تمام شده");
 }
-)
-
-if(stock){
-    alert(" کالا موحود میباشد")
-    console.log(basket)
-}
-
-
-
