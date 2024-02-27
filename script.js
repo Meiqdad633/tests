70de3104f8dd2afa9fee7eb05f13aadc62fee819
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-27 21:42:57 +0330

    new

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-var telegramUsers = [
-    {id:1, name: "Fatemeh", age: 25},
-    {id:2, name: "Meiqdad", age: 26},
-    {id:3, name: "Zahra", age: 30},
-    {id:4, name: "Amir", age: 20},
-    {id:5, name: "Arman", age: 19},
-];
-if (telegramUsers.every((item) => {return item.age>18})){
-    console.log("میتوانید وارد تماس کنفرانسی شوید");
-} else {
-    console.log("اجازه ایجاد تماس کنفرانسی را ندارید");
+/*
+محصولات فرروشگاهی را در داخل ارایه ذخیره کرده و یک ارایه برای سبد خرید در نظر بگیرید 
+ و داخل سبد خرید 3 محصول دیفالت قرار دهید
+از کاربر ورودی بگیرید اگر وردی عدد یک بود حدف محصول اگر 2 اضافه کردن محصول به سبد خرید انجام شود
+*/
+products = [
+    {id:0, name: "pen",price:100},
+    {id:1, name: "book",price:200},
+    {id:2,name: "notebook",price:150},
+    {id:3,name: "pencil",price:170},
+    {id:4,name: "bill",price:400},
+    {id:5,name: "mobile",price:370}
+
+]
+
+basket =[
+    {id:0, name: "pen",price:100},
+    {id:1,name: "bill",price:400},
+    {id:2,name: "mobile",price:370}
+]
+
+input = Number(prompt("1- for add \n2- for delete"))
+
+if(Number(input === 1)){
+    productname = prompt("Enter productname to add")
+    addproduct = products.some((product)=>{
+        if(product.name == productname){
+            basket.push(product)
+            return true
+            }})
+    if(addproduct){
+        alert("محصول به سبد خرید اضافه شد ")
+        basket.forEach(element => {
+            document.write(`${element.id+1}: ${element.name} \n`)
+        });}
+    else{
+            alert("کالا موجود نمی باشد")
+        }
+    }
+
+else if (input === 2){
+     productname = prompt("Enter productname to delete")
+     productindex = basket.findIndex((product)=>{
+         return product.name == productname
+     })
+     if(productindex == -1){
+        alert("کالا موجود نمی باشد")
+     }
+     else{
+        basket.splice(productindex, 1)
+     }
+    }
+else{
+    alert("wrong input")
 }
