33b440fc4e39def96a7b754066259abdae5847cb
Meiqdad <meiqdadhassani12@gmail.com>
2024-02-27 22:47:06 +0330

    new

--- script.js
+++ script.js
@@ -1,55 +1,28 @@
 /*
-محصولات فرروشگاهی را در داخل ارایه ذخیره کرده و یک ارایه برای سبد خرید در نظر بگیرید 
- و داخل سبد خرید 3 محصول دیفالت قرار دهید
-از کاربر ورودی بگیرید اگر وردی عدد یک بود حدف محصول اگر 2 اضافه کردن محصول به سبد خرید انجام شود
+یک فروشگاه انلاین رو پیاده سازی کنید که یک سبد خرید با 6 محصول داشته باشد
+محصولاتی که بالای 100 تومان باشد هزینه پست دریافت نشود و اما محصولات که زیر 100 تومان قیمت دارند هر کدام 10 تومان هزینه پست دریافت شود
+و در اخر قیمت کل سبد خرید با هزینه پست محاسبه شود
+
 */
-products = [
+
+basket = [
     {id:0, name: "pen",price:100},
     {id:1, name: "book",price:200},
-    {id:2,name: "notebook",price:150},
-    {id:3,name: "pencil",price:170},
-    {id:4,name: "bill",price:400},
-    {id:5,name: "mobile",price:370}
+    {id:2,name: "notebook",price:80},
+    {id:3,name: "pencil",price:60},
+    {id:4,name: "bill",price:110},
+    {id:5,name: "mobile",price:30}
+    ]
 
-]
-
-basket =[
-    {id:0, name: "pen",price:100},
-    {id:1,name: "bill",price:400},
-    {id:2,name: "mobile",price:370}
-]
-
-input = Number(prompt("1- for add \n2- for delete"))
-
-if(Number(input === 1)){
-    productname = prompt("Enter productname to add")
-    addproduct = products.some((product)=>{
-        if(product.name == productname){
-            basket.push(product)
-            return true
-            }})
-    if(addproduct){
-        alert("محصول به سبد خرید اضافه شد ")
-        basket.forEach(element => {
-            document.write(`${element.id+1}: ${element.name} \n`)
-        });}
-    else{
-            alert("کالا موجود نمی باشد")
-        }
+sum = 0
+postpayment  = basket.filter((item)=>{
+    if (item.price < 100){
+        document.write(`product with 10$ shiping:  ${item.name} ${"<br>"}`)
+        item.price += 10
     }
+    document.write(`product without shiping:  ${item.name} ${"<br>"}`)
+    sum+=item.price
 
-else if (input === 2){
-     productname = prompt("Enter productname to delete")
-     productindex = basket.findIndex((product)=>{
-         return product.name == productname
-     })
-     if(productindex == -1){
-        alert("کالا موجود نمی باشد")
-     }
-     else{
-        basket.splice(productindex, 1)
-     }
-    }
-else{
-    alert("wrong input")
-}
+})
+
+document.write("the Total apyment with shiping is: ", sum)
