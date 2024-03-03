6cb397d44c67a0b5e6c1e0e33d8628a1e61df00a
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-03 22:06:24 +0330

    new

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-/*
-کلمه ای را از کاربر دریافت کرده و چک کنید که کلمه وارد شده از هردو سمت چپ و راست به یک صورت خوانده میشود
-*/
 
-input = prompt("Enter the Palindrome Word")
-palindrome = input.split("").reverse().join('') == input?document.write("the Palindrome Word is:  ",input):alert("Sorry, Wrong word")
+todolist = [
+    {id:0, name: "reading", done:false},
+    {id:1, name: "writing", done:true},
+    {id:2, name: "speaking", done:false},
+    {id:3, name: "listining", done:true}
+]
+new_item = {id:todolist.length , name: null, status: false}
+
+input = prompt("select \n 1-to add \n 2-for delete \n 3- change status ")
+if(input == 1){
+    input =prompt("Enter the todo name: ")
+    check = todolist.some((item)=>{return item.name == input})?alert("item exist"):new_item.name = input, todolist.push(new_item)
+    console.log(todolist)
     
-
-
+} else if(input == 2){
+    input = (prompt("Enter the name to delete: "))
+    remove = todolist.findIndex((item)=>{ return item.name == todolist.name})
+    todolist.splice(remove, 1)
+    console.log(todolist)
+} else if(input == 3){
+    todolist.forEach(element => { element.status?alert("Done"):element.status = true 
+    });
+    console.log(todolist)
+} else{
+    alert("wrong input")
+}
