ef8487866edee68ac24f0c9f4632b38d69cd33f8
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-04 23:01:25 +0330

    new

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-tasks = {
-    ali:["download", "upload", "copy", "cut"],
-    ahmad:["download", "modify", "read", "cut"],
-    fatemeh:["download", "edit", "reset", "cut"],
-    meiqdad:["download", "upload", "config", "cut"] 
-}
+/*
+یک پروژه ماشین حساب پیاده سازی کنید
+به این ص ورت که 2 عدد را  با همراه عملیات (ضرب و تقسیم....) از کاربر دریافت کرده و عملیات دلخواه کاربر را از روی اعداد ورودی اعمال کند و حال رو نمایش دهد
+*/
+num1 = parseInt(prompt("Num1: ")) , num2 = parseInt(prompt("Num2: "));
+op = prompt("Operation ");
 
-task = prompt("write the task name"), employeer = prompt("assign to")
-employeer in tasks? tasks[employeer].push(task): alert("wrong input");
-console.log(tasks)
+(op === "+") ? document.write(num1 + num2) :
+(op === "-") ? document.write(num1 - num2) :
+(op === "*") ? document.write(num1 * num2) :
+(op === "/") ? document.write(num1 / num2) :
+document.write("Invalid operation");
