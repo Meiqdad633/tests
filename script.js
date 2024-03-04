7fbfc5852f939f3e1618edc3624e40567ad19b02
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-04 23:32:58 +0330

    new

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 فرض کنید در سامانه ای راه اندازی کرده اید، کاربری پسوردش رو فراموش کرده و شما از کاربر یوزرنیم رو  میگیرین و پسورد رو بهش نمایش میدین
 اگر کاربر با یوزرنیم وارد شده در دیتابیش شما وجود نداشته باشد به کاربر پیغام متناسب را نمایش دهید
 */
+input = prompt("Username to reset password")
+users = [
+  {id:0, name: "ahmad", pass:"123"},
+  {id:1, name: "fatemeh", pass:"mylove"},
+  {id:2, name: "meiqdad", pass:"321"},
+]
+
+user = users.find((u)=>{return u.name == input})
+user?document.write(user.pass):alert('user not found')
