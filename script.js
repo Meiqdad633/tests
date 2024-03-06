5e42584c122a1f9cc342a353d53a9099ac6b5629
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-06 15:15:29 +0330

    new

--- script.js
+++ script.js
@@ -3,4 +3,13 @@
 به طوری که از کاربر میزان  ثانیه و دقیقه را دریافت کردهو تایمری را با اطلاعات وارد شده استارت بزنید
 و زمانی که تایمر تمام شد پیغام مناسبی رانمایش دهید
 */
+seconds  = parseInt(prompt("timer seconds: ")), minute = parseInt(prompt("timer minutes: "))
 
+totalSeconds =  (minute * 60) + seconds
+
+console.log(totalSeconds) 
+setInterval(()=>{
+  document.write(totalSeconds)
+  totalSeconds--
+  totalSeconds === 0? clearInterval:"finish"
+})
