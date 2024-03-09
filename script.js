9bbc094a28047ec5495708786ac5ed64048ed4f0
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-09 09:33:23 +0330

    new

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/*
-یک تایمر پیاده سازی کنید
-به طوری که از کاربر میزان  ثانیه و دقیقه را دریافت کردهو تایمری را با اطلاعات وارد شده استارت بزنید
-و زمانی که تایمر تمام شد پیغام مناسبی رانمایش دهید
-*/
-seconds  = parseInt(prompt("timer seconds: ")), minute = parseInt(prompt("timer minutes: "))
+input =prompt("you have 5 secends to upload photo")
 
-totalSeconds = minute * 60
-
-console.log(totalSeconds) 
-setInterval(()=>{
-  totalSeconds--
-  document.write(totalSeconds, "<br>")
-  totalSeconds === 0? (clearInterval(totalSeconds),alert("finished")):""
-}, seconds * 1000)
+setTimeout((e)=>{
+    !e?alert("photo uploading failed"):''
+},5000,input)
