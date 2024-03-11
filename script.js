7a73e59244f945d2694bb615c8d55fe98dc58f43
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-11 20:43:48 +0330

    new

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 /*
 یک لامپ با یک دکمه طراحی کنید که که با کلیک کردن روی دکمه لامپ روشن  و خاموش بشه
 */
+lamp = document.getElementById("lamp")
+lampStatus = false
+function change(){
+    if(!lampStatus){
+        lamp.setAttribute("src", "./on.jpg")
+        lampStatus = true
+    } else{
+        lamp.setAttribute("src", "./off.jpg")
+        lampStatus = false
+    } 
+
+}
+
