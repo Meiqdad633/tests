ad2ec1bcb48bb0fe9624dae87b3a6877afcc94e1
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-14 13:27:24 +0330

    new

--- script.js
+++ script.js
@@ -3,6 +3,25 @@
 */
 let userName= document.querySelector(".username")
 let userpass = document.querySelector(".password")
+function lenUser(){
+    let user = Number(userName.value.length)
+    if(user >= 12){
+        document.getElementById("checkuser").innerHTML = "✔️";
+    }else{
+        document.getElementById("checkuser").innerHTML = "❌";
+    }
+
+}
+function lenPass(){
+    let pass= Number(userpass.value.length)
+    if(pass >= 12){
+        document.getElementById("checkpass").innerHTML = "✔️";
+    }else{
+        document.getElementById("checkpass").innerHTML = "❌";
+    }
+
+}
+
 
 function validation(){
     let user = Number(userName.value.length)
@@ -14,3 +33,5 @@ function validation(){
         alert("Login failed")
     }
 }
+
+
