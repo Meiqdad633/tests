52347499d3fb4cb5fbbbe952d04e987454a28ed4
Meiqdad <meiqdadhassani12@gmail.com>
2024-03-16 23:40:42 +0330

    new

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 
-let userName= document.querySelector(".username")
-let userpass = document.querySelector(".password")
-function lenUser(){
-    let user = Number(userName.value.length)
-    if(user >= 12){
-        document.getElementById("checkuser").innerHTML = "✔️";
-    }else{
-        document.getElementById("checkuser").innerHTML = "❌";
-    }
 
-}
-function lenPass(){
-    let pass= Number(userpass.value.length)
-    if(pass >= 12){
-        document.getElementById("checkpass").innerHTML = "✔️";
-    }else{
-        document.getElementById("checkpass").innerHTML = "❌";
-    }
-
+function randomly(){
+    return(Math.floor(Math.random() *255))
 }
 
 
-function validation(){
-    let user = Number(userName.value.length)
-    let pass= Number(userpass.value.length)
-    if(user >= 12 && pass >= 8){
-        alert("ok")
-    }
-    else{
-        alert("Login failed")
-    }
-}
+let background = document.getElementById("main")
+let heading1 = document.getElementById('headung1')
+heading1.addEventListener('click', setInterval(()=>{
+    background.style.backgroundColor = 'rgba('
+    + randomly() + ',' + randomly()
+    + ',' + randomly() + '\)'
+
+}, 5000))
 
 
